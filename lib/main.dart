a84bcd76dbdb3bd24efa14ed802ec90683fb77ea
Kristiyana <kristiyana.petrova@student.fontys.nl>
2026-03-13 17:40:14 +0100

    added poi and make it possible for the user to go to them

--- lib/main.dart
+++ lib/main.dart
@@ -6,41 +6,78 @@ import 'package:geolocator/geolocator.dart'; // GPS functionality
 void main() {
   runApp(MaterialApp(home: MyMapPage()));
 }
+
 class MyMapPage extends StatefulWidget { // StatefulWidget - widget that can change over time
   const MyMapPage({super.key});
-
   @override
   State<MyMapPage> createState() => _MyMapPageState(); // creates the state for this widget, which is defined in the _MyMapPageState class
 }
 
 class _MyMapPageState extends State<MyMapPage> { // this class holds the state of the MyMapPage widget, including the current position and the logic to update it
-  LatLng myPosition = LatLng(51.4416, 5.4897); // initial position
 
+  LatLng myPosition = LatLng(51.4416, 5.4897); // initial position
+  final List<LatLng> poi = [ // POI; LatLng - class, represents a geographical point with latitude and longitude
+    const LatLng(51.4485, 5.4571), // Strijp-S
+    const LatLng(51.4411, 5.4772), // The Blob
+    const LatLng(51.4417, 5.4674), // Philips Stadium
+  ];
   @override
-  void initState() { // auto-start
-    super.initState();
-    _startTracking(); // start the automatic GPS stream
+  void initState() { // initialization method, called once when the widget is first created,
+    // used to set up any necessary state or start any processes that should run when the widget is displayed
+    super.initState(); // standard background setup
+    // !! ALWAYS CALL SUPER.INITSTATE() FIRST !!
+    _initLocation();
   }
 
+  void _initLocation() async {
+    await _determinePosition(); // shows the request box
+    _startTracking();
+  }
+
+  // Tracking user's location
   void _startTracking() { //tells the app to listen to the GPS constantly
     Geolocator.getPositionStream(
       locationSettings: const LocationSettings(
         accuracy: LocationAccuracy.high,
-        distanceFilter: 5, // updates every 5 meters moved
+        // temp variable for testing
+        distanceFilter: 0,
+        // distanceFilter: 5, // updates every 5 meters moved
       ),
     ).listen((Position position) { // Every time the phone moves the code runs automatically
-      setState(() {
-        myPosition = LatLng(position.latitude, position.longitude);
+      LatLng newPoint = LatLng(position.latitude, position.longitude); // converts new position to LatLng format
+
+      // check every hub in the list
+      for (var hub in poi) {
+        if (checkIfInsideHub(newPoint, hub)) { // checks if the new point is within 50m of the hub
+          print("You are close to a Hub!");
+        }
+      }
+
+      setState(() { // updates the position
+        myPosition = newPoint;
       });
     });
   }
 
+  // Check if player is within 50 meters of a POI
+  bool checkIfInsideHub(LatLng playerPos, LatLng poiPos) {
+    double distance = Geolocator.distanceBetween( // double - Double-Precision Floating Point - n with decimals
+      // distanceBetween - calculates the distance in meters between two geographical points - the Haversine formula
+      playerPos.latitude,
+      playerPos.longitude,
+      poiPos.latitude,
+      poiPos.longitude,
+    );
+    return distance < 50;
+  }
+
+
   Future<Position> _determinePosition() async {// Future - this function will return a position later
-    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled(); // checks if the location services are enabled
     if (!serviceEnabled) {
       return Future.error('Location services are disabled.');
     }
-
+    // Check for permissions
     LocationPermission permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
       return Future.error('Location permission denied.');
@@ -51,7 +88,7 @@ class _MyMapPageState extends State<MyMapPage> { // this class holds the state o
 
     return await Geolocator.getCurrentPosition();
   }
-
+  // This function is not used in the current implementation, but it can be called to get the current position once, instead of listening to a stream of updates. It checks for permissions and returns the current position if everything is in order.
   void updateLocation() async {
     Position position = await _determinePosition();
     setState(() {
@@ -83,6 +120,20 @@ class _MyMapPageState extends State<MyMapPage> { // this class holds the state o
                 ),
               ),
             ],
+          ),
+          MarkerLayer(
+            markers: poi.map((poiLocation) { // poi- list of elements, map - goes through each element, in the () - what is currently the loop holding
+              return Marker(
+                point: poiLocation, // where the marker should be placed
+                width: 60,
+                height: 60,
+                child: const Icon(
+                  Icons.flag_circle, // flag icon
+                  color: Colors.orangeAccent,
+                  size: 35,
+                ),
+              );
+            }).toList(), // converts the iterable returned by map into a list, required by the markers property of MarkerLayer
           ),
         ],
       ),
